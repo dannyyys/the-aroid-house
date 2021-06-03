2bca14c9f65b162d382e256338bbd1b5af111fd8
dannyyys <dannyyys@users.noreply.github.com>
2021-06-03 23:12:28 +0800

    feat: :sparkles: Add delivery option component

---
+++ src/components/checkout/DeliveryOption.js
@@ -0,0 +1,34 @@
+import { Flex, Spacer, chakra, Checkbox, Divider } from "@chakra-ui/react";
+import DeleteButton from "../buttons/DeleteButton";
+
+const DeliveryOption = () => {
+  return (
+    <Flex
+      direction="column"
+      paddingLeft="5"
+      width="80vw"
+      paddingTop="2"
+      paddingBottom="2"
+    >
+      <Flex direction="row">
+        <Checkbox color="white" size="lg">
+          Self collect
+        </Checkbox>
+        <Spacer />
+        <chakra.h3 fontSize="md" color="white">
+          $0
+        </chakra.h3>
+      </Flex>
+      <Flex direction="row">
+        <Checkbox color="white" size="lg">
+          Delivery
+        </Checkbox>
+        <Spacer />
+        <chakra.h3 fontSize="md" color="white">
+          $10
+        </chakra.h3>
+      </Flex>
+    </Flex>
+  );
+};
+export default DeliveryOption;
