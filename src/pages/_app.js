546b499c463235b7381bf3509eb9822a6adcfdd6
dannyyys <dannyyys@users.noreply.github.com>
2021-06-08 03:05:04 +0800

    feat: :sparkles: Add cart context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,23 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import "@fontsource/merriweather/400.css";
 import "@fontsource/lora/400.css";
+import { CartProvider } from "../context/CartContext";
 
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <CartProvider>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </CartProvider>
   );
 }
 
